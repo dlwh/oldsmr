5620cbd929af69717a8a365b580f2b6ee7b3951f
David Hall <dlwh@stanford.edu>
2008-05-02 03:02:49 -0700

    special support for ranges.

--- testscripts/test.scala
+++ testscripts/test.scala
@@ -2,5 +2,6 @@ import edu.stanford.nlp.smr._;
 import edu.stanford.nlp.smr.defaults._;
 
 val x = new ActorDistributor(3);
-println(x.distribute(1 to 100).map(2 * _).reduce(_+_))
-println((1 to 100).map(2 * _).reduceLeft(_+_))
+println("Go!");
+println((1 to 10000000).map(BigInt(_)).reduceLeft(_+_))
+println(x.distribute(1 to 10000000).map(BigInt(_)).reduce(_+_))
