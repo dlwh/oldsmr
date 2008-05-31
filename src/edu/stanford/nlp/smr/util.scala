fb48daca3b0fe4929ecdef057cd803c0fad56ce9
David Hall <dlwh@stanford.edu>
2008-05-30 20:41:44 -0700

    Licenses and moved the package

--- src/edu/stanford/nlp/smr/util.scala
+++
@@ -1,8 +0,0 @@
-package edu.stanford.nlp.smr;
-
-object Util {
-  def identity[T] = (x : T)  => x;
-  def fMap[T,U](f : T=>U) : (Iterable[T])=>Iterable[U] = (x : Iterable[T]) => x.map(f);
-  def fFlatMap[T,U](f : T=>Iterable[U]) : (Iterable[T])=>Iterable[U] = (x : Iterable[T]) => x.flatMap(f);
-  def fFilter[T](f : T=>Boolean) : (Iterable[T])=>Iterable[T] = (x : Iterable[T]) => x.filter(f);
-}
