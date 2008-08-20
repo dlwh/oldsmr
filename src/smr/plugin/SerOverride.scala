a855ade05d0619993f5312c1f4189ec9a72a181d
dlwh <dlwh@c9c46bb1-2420-0410-b6d5-8c78f6715463>
2008-08-20 07:43:55 +0000

    Added a compiler plugin to make writing distributed code.
    git-svn-id: svn+ssh://jacob.stanford.edu/u/nlp/svnroot/trunk/scalanlp/projects/smr@22162 c9c46bb1-2420-0410-b6d5-8c78f6715463

---
+++ src/smr/plugin/SerOverride.scala
@@ -0,0 +1,35 @@
+package smr.plugin;
+import scala.tools.nsc
+import nsc.Global
+import nsc.Phase
+import nsc.plugins.Plugin
+import nsc.plugins.PluginComponent
+
+class SerOverride(val global: Global) extends Plugin {
+  import global._
+
+  val name = "seroverride"
+  val description = "Makes all closures serializable"
+  val components = List[PluginComponent](Component)
+  
+  private object Component extends PluginComponent {
+    val global = SerOverride.this.global
+    val runsAfter = "mixin"
+    val phaseName = SerOverride.this.name
+    def newPhase(prev: Phase) = new SerOverridePhase(prev)    
+  }
+  
+  private class SerOverride(prev: Phase) extends Phase(prev) {
+    def name = SerOverride.this.name
+    def run {
+      for (unit <- currentRun.units; 
+           cdef @ ClassDef(_,name,tparams,impl) <- unit.body) {
+        val sym = cdef.symbol 
+        if(sym.name.toString.contains("anonfun$")) {
+          sym.attributes= AnnotationInfo(definitions.SerializableAttr.tpe, List(), List()) :: sym.attributes 
+        }
+      }
+    }
+  }
+}
+
